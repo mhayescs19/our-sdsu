42d0379ec01026f18f97d89e664b7fe0515463d1
mhayescs19 <mhayescs19@gmail.com>
2025-04-26 15:17:30 -0700

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="fagunExtraBold-title font-normal">Homepage!</div>;
+  return (
+    <div className="fagunExtraBold-title font-normal flex justify-center items-center">
+      Welcome to our site!
+    </div>
+  );
 }
