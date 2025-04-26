47858a3aad41eb14245b69373b381cb138299132
mhayescs19 <mhayescs19@gmail.com>
2025-04-26 15:17:39 -0700

    add demo image page

---
+++ app/classes/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+export default function Classes() {
+  return (
+    <Image
+      src="/oursdsu.png"
+      alt="our.sdsu"
+      width={1512 * 0.97}
+      height={982 * 0.97}
+    />
+  );
+}
